9a4d23890eceec303a2a3e8f2997c22fc5c78920
Felix Vo <ngocvt.dev@gmail.com>
2022-07-31 09:00:21 +0800

    chore: remove filesystem_watchers nvimtree option

--- lua/nvimtree.lua
+++ lua/nvimtree.lua
@@ -113,10 +113,6 @@ require 'nvim-tree'.setup {
         custom = {},
         exclude = {},
     },
-    filesystem_watchers = {
-        enable = false,
-        interval = 100,
-    },
     git = {
         enable = true,
         ignore = true,
