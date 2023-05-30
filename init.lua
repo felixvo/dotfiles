a6c2a4f0a5a4307f9010cddc780848e4e1f72c36
felixvo <felix.vo@viki.com>
2023-05-30 22:42:06 +0800

    add projectionist

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ require('packer').startup({ function(use)
     use 'tpope/vim-fugitive'
     use 'junegunn/gv.vim'
 
+    use 'tpope/vim-projectionist'
     -- common plugins
     use { "ellisonleao/glow.nvim", branch = 'main' }
     use 'numToStr/Comment.nvim'
