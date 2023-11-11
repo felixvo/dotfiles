f8251c3bd4c372577bd8bbc8c87b914006ffd571
Felix Vo <ngocvt.dev@gmail.com>
2023-11-11 23:31:50 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -492,5 +492,4 @@ telescope.load_extension('fzf')
 -- To get telescope-file-browser loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
 telescope.load_extension('file_browser')
-telescope.load_extension('projects')
 
