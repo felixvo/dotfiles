d80e983b8f50ac5c71cc36ebd607852f212861d2
felix.vo <felix.vo@okg.com>
2023-11-19 11:29:38 +0800

    add lazyvim

---
+++ lazyvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
